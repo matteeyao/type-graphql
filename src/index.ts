4a34987da36058d037aeb9d7e03db383e8ba9506
matteeyao <myaoski@gmail.com>
2022-02-16 16:33:24 -0600

    Update tsconfig.json

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import "reflect-metadata";
 import { ApolloServer } from "apollo-server-express";
-import * as Express from "express";
+import Express from "express";
 import { buildSchema, Resolver, Query } from "type-graphql";
 
 @Resolver()
