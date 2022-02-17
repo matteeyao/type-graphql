a2befa9ee8aa286b2466bb1efab6edc9f02070a3
matteeyao <myaoski@gmail.com>
2022-02-17 08:03:50 -0600

    Add auth middleware

--- src/modules/user/Register.ts
+++ src/modules/user/Register.ts
@@ -3,14 +3,19 @@ import {
   Query,
   Mutation,
   Arg,
+  UseMiddleware,
 } from "type-graphql";
 import bcrypt from "bcryptjs";
 
 import { User } from "../../entity/User";
 import { RegisterInput } from "./register/RegisterInput";
+import { isAuth } from "../middleware/isAuth";
+import { logger } from "../middleware/logger";
 
 @Resolver()
 export class RegisterResolver {
+  //Authenticated()
+  @UseMiddleware(isAuth, logger)
   @Query(() => String)
   async hello() {
     return "Hello World!";
